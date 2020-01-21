22acb27a3e9a20e44b181a8a543f7eb85cb4dbfd
Oskar Staudinger <os@s2Eng.onMicrosoft.com>
2020-01-21 15:28:40 +0100

    PMDS.Core.print.PrinterSettings.cs Bullzip und Funktionen entfernt.

--- PMDS.Global/print/PrinterSettings.cs
+++ PMDS.Global/print/PrinterSettings.cs
@@ -40,124 +40,124 @@ namespace PMDS.Print.CR
 
 
 
-        public static bool ConvertPdfToPdfA(string FilePdf, string FilePdfA)
-        {
+        //public static bool ConvertPdfToPdfA(string FilePdf, string FilePdfA)
+        //{
 
-            try
-            {
-                string printerName = PdfUtil.DefaultPrinterName;
-                string errorMessage = "";
+        //    try
+        //    {
+        //        string printerName = PdfUtil.DefaultPrinterName;
+        //        string errorMessage = "";
 
-                bool isError = false;
+        //        bool isError = false;
 
-                PdfSettings settings = new PdfSettings();
-                settings.PrinterName = printerName;
-                settings.SetValue("Output", FilePdfA);
-                settings.SetValue("ShowSettings", "never");
-                settings.SetValue("ShowSaveAS", "never");
-                settings.SetValue("ShowProgress", "no");
-                settings.SetValue("ShowProgressFinished", "no");
-                settings.SetValue("ShowPDF", "no");
-                settings.SetValue("ConfirmOverwrite", "no");
-                settings.SetValue("Format", "pdfa1b");
+        //        PdfSettings settings = new PdfSettings();
+        //        settings.PrinterName = printerName;
+        //        settings.SetValue("Output", FilePdfA);
+        //        settings.SetValue("ShowSettings", "never");
+        //        settings.SetValue("ShowSaveAS", "never");
+        //        settings.SetValue("ShowProgress", "no");
+        //        settings.SetValue("ShowProgressFinished", "no");
+        //        settings.SetValue("ShowPDF", "no");
+        //        settings.SetValue("ConfirmOverwrite", "no");
+        //        settings.SetValue("Format", "pdfa1b");
 
-                // Get the name of a status file and delete it if it already exist
-                string statusFileName = Path.Combine(Path.GetTempPath(), System.Guid.NewGuid().ToString());
-                if (File.Exists(statusFileName))
-                    File.Delete(statusFileName);
-                settings.SetValue("StatusFile", statusFileName);
-                settings.WriteSettings(PdfSettingsFileType.RunOnce);
+        //        // Get the name of a status file and delete it if it already exist
+        //        string statusFileName = Path.Combine(Path.GetTempPath(), System.Guid.NewGuid().ToString());
+        //        if (File.Exists(statusFileName))
+        //            File.Delete(statusFileName);
+        //        settings.SetValue("StatusFile", statusFileName);
+        //        settings.WriteSettings(PdfSettingsFileType.RunOnce);
 
-                try
-                {
-                    PdfUtil.PrintFile(FilePdf, printerName);
-                    PdfUtil.WaitForFile(statusFileName, 60000);
-                    isError = !File.Exists(FilePdfA);
-                }
+        //        try
+        //        {
+        //            PdfUtil.PrintFile(FilePdf, printerName);
+        //            PdfUtil.WaitForFile(statusFileName, 60000);
+        //            isError = !File.Exists(FilePdfA);
+        //        }
 
-                catch (Exception ex)
-                {
-                    isError = true;
-                    errorMessage = ex.Message;
-                }
+        //        catch (Exception ex)
+        //        {
+        //            isError = true;
+        //            errorMessage = ex.Message;
+        //        }
 
-                if (!isError)
-                {
-                    File.Delete(FilePdf);
-                    if (File.Exists(statusFileName))
-                        File.Delete(statusFileName);
-                    return true;
-                }
-                else
-                    return false;
+        //        if (!isError)
+        //        {
+        //            File.Delete(FilePdf);
+        //            if (File.Exists(statusFileName))
+        //                File.Delete(statusFileName);
+        //            return true;
+        //        }
+        //        else
+        //            return false;
 
-            }
+        //    }
 
-            catch (Exception ex)
-            {
-                throw new Exception("PrinterSettings.ConvertPdfToPdfA: " + ex.ToString());
-            }
-        }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception("PrinterSettings.ConvertPdfToPdfA: " + ex.ToString());
+        //    }
+        //}
 
       
-        public static Boolean PrintToBullZip(string FileName, bool pdfa)
-        {
-            try
-            {
-                Boolean BullZipInstalled = false;
-                for (int i = 0; i < System.Drawing.Printing.PrinterSettings.InstalledPrinters.Count; i++)
-                {
-                    if (System.Drawing.Printing.PrinterSettings.InstalledPrinters[i].ToString().Equals("bullzip pdf printer", StringComparison.CurrentCultureIgnoreCase))
-                    {
-                        BullZipInstalled = true;
-                        break;
-                    }
-                }
+        //public static Boolean PrintToBullZip(string FileName, bool pdfa)
+        //{
+        //    try
+        //    {
+        //        Boolean BullZipInstalled = false;
+        //        for (int i = 0; i < System.Drawing.Printing.PrinterSettings.InstalledPrinters.Count; i++)
+        //        {
+        //            if (System.Drawing.Printing.PrinterSettings.InstalledPrinters[i].ToString().Equals("bullzip pdf printer", StringComparison.CurrentCultureIgnoreCase))
+        //            {
+        //                BullZipInstalled = true;
+        //                break;
+        //            }
+        //        }
 
 
 
-                if (BullZipInstalled)
-                {
-                    Bullzip.PdfWriter.PdfSettings bz = new Bullzip.PdfWriter.PdfSettings();
-                    bz.Init();
-                    bz.PrinterName = PdfUtil.DefaultPrinterName;
-                    bz.SetValue("output", FileName);
-                    bz.SetValue("suppresserrors", "no");
-                    bz.SetValue("openfolder", "no");
-                    bz.SetValue("rememberlastfilename", "no");
-                    bz.SetValue("rememberlastfoldername", "no");
-                    bz.SetValue("res", "150");
-                    bz.SetValue("resx", "150");
-                    bz.SetValue("resy", "150");
-                    bz.SetValue("textalphabits", "4");
-                    bz.SetValue("graphicsalphabits", "4");
-                    bz.SetValue("linearize", "no");
-                    bz.SetValue("usethumbs", "no");
-                    bz.SetValue("macroscripttimeout", "120");
-                    bz.SetValue("printencrypted", "no");
-                    bz.SetValue("appendifexists", "no");
-                    bz.SetValue("confirmoverwrite", "no");
-                    bz.SetValue("usedefaultauthor", "no");
-                    bz.SetValue("usedefaulttitle", "no");
-                    bz.SetValue("rememberlastoptionset", "yes");
-                    bz.SetValue("showsaveas", "never");
-                    bz.SetValue("showsettings", "never");
-                    bz.SetValue("showpdf", "never");
-                    if (pdfa)
-                        bz.SetValue("Format", "pdfa1b");
-                    bz.WriteSettings(PdfSettingsFileType.RunOnce);
+        //        if (BullZipInstalled)
+        //        {
+        //            Bullzip.PdfWriter.PdfSettings bz = new Bullzip.PdfWriter.PdfSettings();
+        //            bz.Init();
+        //            bz.PrinterName = PdfUtil.DefaultPrinterName;
+        //            bz.SetValue("output", FileName);
+        //            bz.SetValue("suppresserrors", "no");
+        //            bz.SetValue("openfolder", "no");
+        //            bz.SetValue("rememberlastfilename", "no");
+        //            bz.SetValue("rememberlastfoldername", "no");
+        //            bz.SetValue("res", "150");
+        //            bz.SetValue("resx", "150");
+        //            bz.SetValue("resy", "150");
+        //            bz.SetValue("textalphabits", "4");
+        //            bz.SetValue("graphicsalphabits", "4");
+        //            bz.SetValue("linearize", "no");
+        //            bz.SetValue("usethumbs", "no");
+        //            bz.SetValue("macroscripttimeout", "120");
+        //            bz.SetValue("printencrypted", "no");
+        //            bz.SetValue("appendifexists", "no");
+        //            bz.SetValue("confirmoverwrite", "no");
+        //            bz.SetValue("usedefaultauthor", "no");
+        //            bz.SetValue("usedefaulttitle", "no");
+        //            bz.SetValue("rememberlastoptionset", "yes");
+        //            bz.SetValue("showsaveas", "never");
+        //            bz.SetValue("showsettings", "never");
+        //            bz.SetValue("showpdf", "never");
+        //            if (pdfa)
+        //                bz.SetValue("Format", "pdfa1b");
+        //            bz.WriteSettings(PdfSettingsFileType.RunOnce);
 
-                    SetDefaultPrinterName(bz.PrinterName);
-                }
-                return BullZipInstalled;
+        //            SetDefaultPrinterName(bz.PrinterName);
+        //        }
+        //        return BullZipInstalled;
 
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("PrinterSettings.PrintToBullZip: " + ex.ToString());
-            }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception("PrinterSettings.PrintToBullZip: " + ex.ToString());
+        //    }
 
-        }
+        //}
 
 
         public static bool WaitForFile(string FileName)
