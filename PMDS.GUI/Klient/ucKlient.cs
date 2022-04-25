9f31a9b97abd67fb63335c2227a3ca512839a69b
Oskar Staudinger <os@s2Eng.onMicrosoft.com>
2022-04-25 10:34:15 +0200

    Kontrolle Performance Auswahllisten

--- PMDS.GUI/Klient/ucKlient.cs
+++ PMDS.GUI/Klient/ucKlient.cs
@@ -10,8 +10,7 @@ using PMDS.Global;
 using PMDS.BusinessLogic;
 using PMDS.GUI.BaseControls;
 using System.Linq;
-
-
+using System.Threading.Tasks;
 
 namespace PMDS.GUI
 {
@@ -183,19 +182,6 @@ namespace PMDS.GUI
         public bool ValidateFields()
         {
             return ucKlientStammdaten1.ValidateFields();
-
-            ////Änderung nach 09.05.2007
-            //foreach (UltraTab tab in tabKlientenakt.Tabs)
-            //{
-            //    foreach (Control c in tab.TabPage.Controls)
-            //    {
-            //        if (c is IWizardPage && !((IWizardPage)c).ValidateFields())
-            //        {
-            //            bError = true;
-            //            return !bError;
-            //        }
-            //    }
-            //}
         }
 
         public void Write()
