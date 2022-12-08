9d972361f35b3c00857e3ef2254de9a4dc2410b0
Oskar Staudinger <os@s2Eng.onMicrosoft.com>
2022-12-08 23:34:39 +0100

    Refactor Launcher namespaces korrigiert

--- PMDS.Launcher/ui.cs
+++ PMDS.Launcher/ui.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
-
-public class ui
+namespace Launcher
 {
+    public class ui
+    {
     
 
-    public bool msgBox(string txt, string tit )
-    {
-        MessageBox.Show(txt, tit);
-        return false;
-    }
+        public bool msgBox(string txt, string tit )
+        {
+            MessageBox.Show(txt, tit);
+            return false;
+        }
 
+    }
 }
 
