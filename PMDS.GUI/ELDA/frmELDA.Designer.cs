6ca8f213eba924c19c57a682684a1a7b00a27fa5
Oskar Staudinger <os@s2Eng.onMicrosoft.com>
2023-01-02 10:54:36 +0100

    frmELDA: Icon hinzugefügt, Schrift auf Segoe 10

--- PMDS.GUI/ELDA/frmELDA.Designer.cs
+++ PMDS.GUI/ELDA/frmELDA.Designer.cs
@@ -29,6 +29,7 @@ namespace PMDS.GUI.ELDA
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmELDA));
             this.rtbLog = new System.Windows.Forms.RichTextBox();
             this.btnPickXlsx = new Infragistics.Win.Misc.UltraButton();
             this.btnPickTxt = new Infragistics.Win.Misc.UltraButton();
@@ -48,18 +49,20 @@ namespace PMDS.GUI.ELDA
             // 
             this.rtbLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.rtbLog.Location = new System.Drawing.Point(6, 100);
+            this.rtbLog.Location = new System.Drawing.Point(7, 131);
+            this.rtbLog.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rtbLog.Name = "rtbLog";
-            this.rtbLog.Size = new System.Drawing.Size(784, 236);
+            this.rtbLog.Size = new System.Drawing.Size(914, 307);
             this.rtbLog.TabIndex = 0;
             this.rtbLog.Text = "";
             // 
             // btnPickXlsx
             // 
             this.btnPickXlsx.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnPickXlsx.Location = new System.Drawing.Point(600, 11);
+            this.btnPickXlsx.Location = new System.Drawing.Point(700, 14);
+            this.btnPickXlsx.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPickXlsx.Name = "btnPickXlsx";
-            this.btnPickXlsx.Size = new System.Drawing.Size(92, 22);
+            this.btnPickXlsx.Size = new System.Drawing.Size(107, 29);
             this.btnPickXlsx.TabIndex = 1;
             this.btnPickXlsx.Text = "Suchen";
             this.btnPickXlsx.Click += new System.EventHandler(this.btnPickXlsx_Click);
@@ -67,26 +70,29 @@ namespace PMDS.GUI.ELDA
             // btnPickTxt
             // 
             this.btnPickTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnPickTxt.Location = new System.Drawing.Point(600, 47);
+            this.btnPickTxt.Location = new System.Drawing.Point(700, 61);
+            this.btnPickTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPickTxt.Name = "btnPickTxt";
-            this.btnPickTxt.Size = new System.Drawing.Size(92, 21);
+            this.btnPickTxt.Size = new System.Drawing.Size(107, 27);
             this.btnPickTxt.TabIndex = 2;
             this.btnPickTxt.Text = "Suchen";
             this.btnPickTxt.Click += new System.EventHandler(this.btnPickTxt_Click);
             // 
             // lblXlsx
             // 
-            this.lblXlsx.Location = new System.Drawing.Point(6, 16);
+            this.lblXlsx.Location = new System.Drawing.Point(7, 21);
+            this.lblXlsx.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lblXlsx.Name = "lblXlsx";
-            this.lblXlsx.Size = new System.Drawing.Size(131, 17);
+            this.lblXlsx.Size = new System.Drawing.Size(153, 22);
             this.lblXlsx.TabIndex = 3;
             this.lblXlsx.Text = "Excel-Rechnungs-Datei";
             // 
             // lblTxt
             // 
-            this.lblTxt.Location = new System.Drawing.Point(6, 51);
+            this.lblTxt.Location = new System.Drawing.Point(7, 67);
+            this.lblTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lblTxt.Name = "lblTxt";
-            this.lblTxt.Size = new System.Drawing.Size(131, 17);
+            this.lblTxt.Size = new System.Drawing.Size(153, 22);
             this.lblTxt.TabIndex = 4;
             this.lblTxt.Text = "Text-ELDA-Datei";
             // 
@@ -94,9 +100,10 @@ namespace PMDS.GUI.ELDA
             // 
             this.txtXlsx.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtXlsx.Location = new System.Drawing.Point(135, 12);
+            this.txtXlsx.Location = new System.Drawing.Point(158, 16);
+            this.txtXlsx.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtXlsx.Name = "txtXlsx";
-            this.txtXlsx.Size = new System.Drawing.Size(459, 21);
+            this.txtXlsx.Size = new System.Drawing.Size(536, 26);
             this.txtXlsx.TabIndex = 5;
             this.txtXlsx.ValueChanged += new System.EventHandler(this.txtXlsx_ValueChanged);
             // 
@@ -104,9 +111,10 @@ namespace PMDS.GUI.ELDA
             // 
             this.txtTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtTxt.Location = new System.Drawing.Point(135, 47);
+            this.txtTxt.Location = new System.Drawing.Point(158, 61);
+            this.txtTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtTxt.Name = "txtTxt";
-            this.txtTxt.Size = new System.Drawing.Size(459, 21);
+            this.txtTxt.Size = new System.Drawing.Size(536, 26);
             this.txtTxt.TabIndex = 6;
             this.txtTxt.ValueChanged += new System.EventHandler(this.txtTxt_ValueChanged);
             // 
@@ -114,9 +122,10 @@ namespace PMDS.GUI.ELDA
             // 
             this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnStart.Enabled = false;
-            this.btnStart.Location = new System.Drawing.Point(698, 46);
+            this.btnStart.Location = new System.Drawing.Point(814, 60);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(92, 21);
+            this.btnStart.Size = new System.Drawing.Size(107, 27);
             this.btnStart.TabIndex = 7;
             this.btnStart.Text = "Start";
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
@@ -126,33 +135,36 @@ namespace PMDS.GUI.ELDA
             this.rtbTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.rtbTxt.Location = new System.Drawing.Point(4, 368);
+            this.rtbTxt.Location = new System.Drawing.Point(5, 481);
+            this.rtbTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rtbTxt.Name = "rtbTxt";
-            this.rtbTxt.Size = new System.Drawing.Size(784, 255);
+            this.rtbTxt.Size = new System.Drawing.Size(914, 332);
             this.rtbTxt.TabIndex = 8;
             this.rtbTxt.Text = "";
             // 
             // lblLog
             // 
-            this.lblLog.Location = new System.Drawing.Point(6, 77);
+            this.lblLog.Location = new System.Drawing.Point(7, 101);
+            this.lblLog.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lblLog.Name = "lblLog";
-            this.lblLog.Size = new System.Drawing.Size(131, 17);
+            this.lblLog.Size = new System.Drawing.Size(153, 22);
             this.lblLog.TabIndex = 9;
             this.lblLog.Text = "Log";
             // 
             // lblRex
             // 
-            this.lblRex.Location = new System.Drawing.Point(6, 345);
+            this.lblRex.Location = new System.Drawing.Point(7, 451);
+            this.lblRex.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lblRex.Name = "lblRex";
-            this.lblRex.Size = new System.Drawing.Size(131, 17);
+            this.lblRex.Size = new System.Drawing.Size(153, 22);
             this.lblRex.TabIndex = 10;
             this.lblRex.Text = "Ergebnis";
             // 
             // frmELDA
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 635);
+            this.ClientSize = new System.Drawing.Size(933, 830);
             this.Controls.Add(this.lblRex);
             this.Controls.Add(this.lblLog);
             this.Controls.Add(this.rtbTxt);
@@ -164,8 +176,11 @@ namespace PMDS.GUI.ELDA
             this.Controls.Add(this.btnPickTxt);
             this.Controls.Add(this.btnPickXlsx);
             this.Controls.Add(this.rtbLog);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmELDA";
-            this.Text = "ELDA";
+            this.Text = "ELDA - Abrechnung für nässende Inkontinenzprodukte";
             ((System.ComponentModel.ISupportInitialize)(this.txtXlsx)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtTxt)).EndInit();
             this.ResumeLayout(false);
